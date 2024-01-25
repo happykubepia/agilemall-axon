86cb74f599f1b85dce76049d9946f3b51265bd4c
이해경 <ondal@ihaegyeong-ui-MacBookAir.local>
2024-01-25 11:13:33 +0900

    delete unused

--- report/src/main/java/com/agilemall/report/events/ReportEventsHandler.java
+++ report/src/main/java/com/agilemall/report/events/ReportEventsHandler.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.Optional;
 
 @Slf4j
